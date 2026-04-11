6e80478a72cadd798d1879be4bda6feecb37650b
Valeria Shamykaeva <Big3L@yandex.ru>
2026-04-11 14:08:28 +0500

    DRF API for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Post
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -8,3 +10,7 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
