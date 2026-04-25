b30e0ed6723de47181700cd9d7147e23f39c99c2
Valeria Shamykaeva <Big3L@yandex.ru>
2026-04-25 13:01:15 +0500

    register, login, and logout

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ router.register(r'posts', views.PostViewSet, basename='post')
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('api/v1/', include(router.urls)),
 ]
