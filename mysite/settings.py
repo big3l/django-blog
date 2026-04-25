a08be7189fcfb5108db60274e39d72a056be0871
Valeria Shamykaeva <Big3L@yandex.ru>
2026-04-25 13:58:05 +0500

    message framework

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
